d96cd7f541ddc8a5de4417cef7c45ee7bd8a29ee
Aditya Chauhan <adityachauhan71804@gmail.com>
2025-06-25 09:50:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello World !!!</h1>
+      <h1>Hello World</h1>
+      <p>This is a simple React application.</p>
+      <p>Click the button below to see a message.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+      <p>Enjoy coding with React!</p>
+      <p>Make sure to check out the documentation for more features.</p>
+      <p>Happy coding!</p>
+      <p>Feel free to modify this code as you like.</p>
+      <p>React is a powerful library for building user interfaces.</p>
+      <p>Explore the React ecosystem for additional libraries and tools.</p>
+      <p>React has a vibrant community, so you can find help easily.</p>
+      <p>Keep learning and improving your skills in React.</p>
+      <p>There are many resources available online to help you.</p>
+      <p>Enjoy building amazing applications with React!</p>
     </>
   )
 }
